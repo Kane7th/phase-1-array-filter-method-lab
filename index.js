b4f5ec230123ff72fa4f667ce1ca69ad3dcb858f
Kane Kabena <onekaneldn@gmail.com>
2025-03-12 09:59:27 +0300

    array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const nameLower = name.toLowerCase();
+    return drivers.filter(drivers => drivers.toLowerCase().includes(nameLower));
+}
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+//----------------------------------------------------------------------------------------------------------
+
+function fuzzyMatch(drivers, letters) {
+    const nameLetter = letters.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(nameLetter));
+}
+
+//----------------------------------------------------------------------------------------------------------
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
